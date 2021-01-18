47024f3eaeff1c356168cc48c3d4459a98390e59
clide7029 <70732345+clide7029@users.noreply.github.com>
2021-01-17 21:08:09 -0800

    Finished Adventure game

---
+++ python/Space_Escape/adventure.py
@@ -0,0 +1,180 @@
+
+adv_pageD = {
+    "title"   : "Death",
+    "content" : "You sit in silence and slowly embrace the sweet call of death",
+    "pathA"   : ("Play","Space Escape"),
+    "pathB"   : ("Again?","Space Escape")
+}
+adv_page9A = {
+    "title"   : "Congrats",
+    "content" : "You escaped!",
+    "pathA"   : ("Play", "Space Escape"),
+    "pathB"   : ("Again?", "Space Escape")
+}
+adv_page8A = {
+    "title"   : "Make key",
+    "content" : "The cube pieces seem to magnetically stick together and you assemble a key that looks like it will fit.",
+    "pathA"   : ("Open the door", "Congrats"),
+    "pathB"   : ("Open the door", "Congrats")
+}
+adv_page8B = {
+    "title"   : "More help",
+    "content" : 'The robot tells you, you will never make it out alive even if it helps you.',
+    "pathA"   : ("Try and make the key", "Make key"),
+    "pathB"   : ("Attack the robot", "Attack")
+}
+adv_page7A = {
+    "title"   : "Take pieces",
+    "content" : "The lock on the door is not very cube-like, if this actually is the key it will need to be rearranged somehow.",
+    "pathA"   : ("Try and make the key", "Make key"),
+    "pathB"   : ("Ask the robot for more help", "More help")
+}
+adv_page7B = {
+    "title"   : "Take robot",
+    "content" : "As you reach for the robot it makes small, sharp spikes protrude from its body. It does not want to be touched.",
+    "pathA"   : ("Take the pieces to the door", "Take pieces"),
+    "pathB"   : ("Ask the robot for more help", "More help")
+}
+adv_page6C = {
+    "title"   : "Pull",
+    "content" : "You're holding the cube now, but aren't sure what to do with it.",
+    "pathA"   : ("Set it on the can", "Set can"),
+    "pathB"   : ("Set it on the workbench", "Set workbench")
+}
+adv_page6B = {
+    "title"   : "Ask for help",
+    "content" : "The robot deciphers what is written on the cube and tells you that it says, reform what was never born to find the key that you need. The robot presses the cube to a small circular spot on its face and the cube breaks into a bunch of puzzle pieces.",
+    "pathA"   : ("Take the pieces to the door", "Take pieces"),
+    "pathB"   : ("Take the robot to the door", "Take robot")
+}
+adv_page6A = {
+    "title"   : "Attack",
+    "content" : "The robot vaporizes you instantly",
+    "pathA"   : ("YOU", "Space Escape"),
+    "pathB"   : ("DIED", "Space Escape")
+}
+adv_page5A = {
+    "title"   : "Set can",
+    "content" : 'The cube is drawn to the can and slips out of your hand to stick to it as you get close to putting it down. The can grows metallic appendages and takes the cube off its head. It is clear this is a robot of some kind. It says to you, "human, you have awoken me."',
+    "pathA"   : ("Attack the robot", "Attack"),
+    "pathB"   : ("Ask for help escaping", "Ask for help")
+}
+adv_page5B = {
+    "title"   : "Set workbench",
+    "content" : "As you set it back down you notice the cube is slowly being pulled towards the can.",
+    "pathA"   : ("Set it on the can", "Set can"),
+    "pathB"   : ("pull it away", "pull")
+}
+adv_page4DD = {
+    "title"   : "Too loud",
+    "content" : "The noise of you cumbersomely picking at this lock has attracted the attention of someone. Loud footsteps approach the door before you hear a beep and the room starts to fill with poisonous gas.",
+    "pathA"   : ("YOU","Space Escape"),
+    "pathB"   : ("DIED","Space Escape")
+}
+adv_page4D = {
+    "title"   : "Fiddling",
+    "content" : "Nothing seems to work. The lock resembles a cube-like puzzle.",
+    "pathA"   : ("Keep fiddling", "Too loud"),
+    "pathB"   : ("Look around the room", "Looking")
+}
+adv_page4C = {
+    "title"   : "Inspecting",
+    "content" : "Written around the cube is strange writing you are not able to decipher, but you notice it has a dark circle at its base.",
+    "pathA"   : ("Set it on the can", "Set can"),
+    "pathB"   : ("Set it on the workbench", "Set workbench")
+}
+adv_page4B = {
+    "title"   : "Pulling",
+    "content" : "The cube will not pull apart, but you can it must come apart somehow. ",
+    "pathA"   : ("Get a closer look at the puzzle", "Inspecting"),
+    "pathB"   : ("Set it on the can", "Set can")
+}
+adv_page4A = {
+    "title"   : "Punch",
+    "content" : "The can transforms into a laser shooting robot and vaporizes you.",
+    "pathA"   : ("YOU DIED", "Space Escape"),
+    "pathB"   : ("Play Again?", "Space Escape")
+}
+adv_page3D = {
+    "title"   : "Looking",
+    "content" : "There are no other openings besides this door.",
+    "pathA"   : ("Give up - it's too hard", "Death"),
+    "pathB"   : ("Go to the workbench", "Workbench")
+}
+adv_page3C = {
+    "title"   : "Breakdown",
+    "content" : "The door hardly even rattles. It seems to be very securely held in place.",
+    "pathA"   : ("Try and fiddle with the lock", "Fiddling"),
+    "pathB"   : ("Look around the room", "Looking")
+}
+adv_page3B = {
+    "title"   : "Cube",
+    "content" : "The cube looks to be made of several smaller pieces, but you are not sure how they fit together or what it is for. ",
+    "pathA"   : ("Pull at the pieces", "Pulling"),
+    "pathB"   : ("Get a closer look at the puzzle", "Inspecting")
+}
+adv_page3A = {
+    "title"   : "Can",
+    "content" : "The can will not budge",
+    "pathA"   : ("Punch it", "Punch"),
+    "pathB"   : ("Pickup the cube", "Cube")
+}
+adv_page2B = {
+    "title"   : "Door",
+    "content" : "The door is taller, and wider than most doors you are used to. It is made of some kind of shiny metal and does not have a door knob. Instead there is a small hole that looks like you could put something into it, but it is an odd and specific shape. ",
+    "pathA"   : ("Try and break down the door", "Breakdown"),
+    "pathB"   : ("Look around the room", "Looking")
+}
+adv_page2A = {
+    "title"   : "Workbench",
+    "content" : "On the workbench there is a can and cube",
+    "pathA"   : ("Pickup the can", "Can"),
+    "pathB"   : ("Pickup the cube", "Cube")
+}
+adv_page1 = {
+    "title"   : "Space Escape",
+    "content" : "You awake in a grey metallic room that seems totally alien to you. Immediately you notice there is a desk with some weird gizmos on it and a door.",
+    "pathA"   : ("Go to the workbench", "Workbench"),
+    "pathB"   : ("Go to the door", "Door")
+}
+
+adventure = [adv_page1, adv_page2A, adv_page2B, adv_page3A, adv_page3B, adv_page3C, adv_page3D, adv_page4A, adv_page4B, adv_page4C, adv_page4D, adv_page4DD,
+adv_page5A, adv_page5B, adv_page6A, adv_page6B, adv_page7A, adv_page7B, adv_page8A, adv_page8B, adv_page9A, adv_pageD]
+
+def make_page(adv_page):
+    buttonA = adv_page["pathA"]
+    labelA = buttonA[0]
+    linkA = buttonA[1] + ".html"
+    buttonB = adv_page["pathB"]
+    labelB = buttonB[0]
+    linkB = buttonB[1] + ".html"
+    html = """<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>%s</title>
+    </head>
+
+    <body>
+        <div class="headSpace"></div>
+        <div class="container">
+            <h1>%s</h1>
+            <p>%s</p>
+            <div class="centerButtons">
+                <button> <a href="%s"> %s </a></button>
+                <button> <a href="%s"> %s </a></button>
+            </div>
+        </div>
+    </body>
+    </html>
+    """ % (adv_page["title"], adv_page["title"], adv_page["content"], 
+        linkA, labelA, linkB, labelB)
+
+    f = open(adv_page["title"] + ".html","w")
+    f.write(html)
+    f.close()
+
+for page in adventure:
+    make_page(page)
